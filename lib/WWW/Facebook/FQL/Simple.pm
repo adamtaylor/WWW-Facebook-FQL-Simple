82adb4cc4f5098b1f7bc81fd5128df24d8ebdc34
Adam Taylor <adamjctaylor@gmail.com>
2011-07-16 21:02:04 +0100

    Add documentation

--- lib/WWW/Facebook/FQL/Simple.pm
+++ lib/WWW/Facebook/FQL/Simple.pm
@@ -1,6 +1,27 @@
 # ABSTRACT: Simple interface for making FQL requests.
 package WWW::Facebook::FQL::Simple;
 
+=head1 SYNOPSIS
+
+    use WWW::Facebook::FQL::Simple;
+
+    WWW::Facebook::FQL::Simple->query(
+        query => 'SELECT like_count FROM link_stat WHERE url="http://twitter.com"'
+    );
+
+=head1 DESCRIPTION
+
+A no nonesense, dead simple interface to for making FQL requests. This module
+does not handle sessions or authentication so presumably some requests will not
+work.
+
+If your needs are more complex, you probably need L<WWW::Facebook::API> or
+L<WWWW::Facebook::FQL>.
+
+=head1 METHODS
+
+=cut
+
 use strict;
 use warnings;
 
@@ -9,6 +30,16 @@ use LWP::UserAgent;
 use URI::Encode qw( uri_encode );
 
 my $API_BASE = 'http://api.facebook.com/method/fql.query?format=json&query=';
+
+=head2 query
+
+    WWW::Facebook::FQL::Simple->query(
+        query => 'SELECT like_count FROM link_stat WHERE url="http://twitter.com"'
+    );
+
+Returns a hash reference of the JSON returned from the API.
+
+=cut
 
 sub query {
     my $class = shift;
@@ -28,5 +59,11 @@ sub query {
     }
 
 }
+
+=head1 SEE ALSO
+
+L<Facebook>, L<WWW::Facebook::API>, L<WWW::Facebook::FQL>
+
+=cut
 
 1;
